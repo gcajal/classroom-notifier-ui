8d7a18875ee20eb86c2856e09517f5d12d9706e1
Nicolas9300 <64173877+Nicolas9300@users.noreply.github.com>
2024-09-19 23:18:21 -0300

    Update ListaView.java

--- src/main/java/_ClassroomNotifierUI/ListaView.java
+++ src/main/java/_ClassroomNotifierUI/ListaView.java
@@ -19,16 +19,14 @@ import classroom.notifier.entity.Observer;
 public class ListaView implements Observer{
 
     private ListaController controller;
-	private JLabel changesLabel;
 	private ClassroomNotifier classroomNotifier;
-
-
-	public JLabel getChangesLabel() {
-		return changesLabel;
-	}
+	private BotonPanel botonPanel;
+	private ChangesLabelPanel changesLabelPanel;
+	private TitlePanel titlePanel;
 
 	public void setController(ListaController controller) {
 		this.controller = controller;
+		botonPanel.getTestButton().addActionListener(e -> controller.handleTestButtonClick());
 	}
 
 	/**
@@ -49,35 +47,17 @@ public class ListaView implements Observer{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JLabel titleLabel = new JLabel("ClassroomNotifier");
-		titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
-		titleLabel.setBounds(150, 10, 200, 30);
-		frame.getContentPane().add(titleLabel);
+		titlePanel = new TitlePanel();
+		titlePanel.setBounds(150, 10, 200, 30);
+		frame.getContentPane().add(titlePanel);
 
-		changesLabel = new JLabel("Se detectaron cambios de aulas");
-		changesLabel.setBounds(220, 80, 200, 30);
-		changesLabel.setVisible(false);
-		frame.getContentPane().add(changesLabel);
+		changesLabelPanel = new ChangesLabelPanel();
+		changesLabelPanel.setBounds(220, 80, 200, 30);
+		frame.getContentPane().add(changesLabelPanel);
 
-		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 6));
-		buttonPanel.setBounds(50, 50, 90, 70);
-		buttonPanel.setBorder(new LineBorder(Color.BLACK, 2));
-		frame.getContentPane().add(buttonPanel);
-
-		buttonPanel.add(Box.createVerticalGlue());
-
-		JButton testButton = new JButton("Test");
-		testButton.setBackground(Color.RED);
-		testButton.setForeground(Color.WHITE);
-		testButton.setFocusPainted(false);
-		testButton.setOpaque(true);
-		testButton.setBorderPainted(false);
-		testButton.setPreferredSize(new Dimension(80, 50));
-		buttonPanel.add(testButton);
-
-		buttonPanel.add(Box.createVerticalGlue());
-
-		testButton.addActionListener(e -> controller.handleTestButtonClick());
+		botonPanel = new BotonPanel();
+		botonPanel.setBounds(50, 50, 90, 70);
+		frame.getContentPane().add(botonPanel);
 
 		frame.setVisible(true);
 	}
@@ -85,5 +65,9 @@ public class ListaView implements Observer{
 	@Override
 	public void update(Observable observable, Object o) {
 
+	}
+
+	public JLabel getChangesLabel() {
+		return changesLabelPanel.getChangesLabel();
 	}
 }
