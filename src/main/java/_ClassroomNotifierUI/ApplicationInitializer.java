6eca7a7f49cf7a0779016dd60d4a382b148159da
Gabriel <gabrielmcajal@gmail.com>
2024-11-09 01:29:33 -0300

    Uso Singleton

--- src/main/java/_ClassroomNotifierUI/ApplicationInitializer.java
+++ src/main/java/_ClassroomNotifierUI/ApplicationInitializer.java
@@ -3,17 +3,21 @@ import classroom.notifier.ClassroomNotifier;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
 
 
 public class ApplicationInitializer {
 
     public static void main(String[] args) {
 
+        System.out.println(new Date().toString());
         //String[] argsClass = new String[1];
-        ClassroomNotifier classroom = new ClassroomNotifier(args);
 
-        FactoryRegistrador factoryRegistrador = new FactoryRegistrador();
-        factoryRegistrador.cargarRegistrador(classroom,args);
+        ClassroomNotifier classroom = ClassroomNotifier.getInstance(args);
+        //ClassroomNotifier classroom = new ClassroomNotifier(args);
+
+        //FactoryRegistrador factoryRegistrador = new FactoryRegistrador();
+        //factoryRegistrador.cargarRegistrador(classroom,args);
 
         new ListaView(classroom);
     }
